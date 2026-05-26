cb9f5398c056955063556cdc8538984a522e3c3f
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-26 22:04:17 +0200

    Update app.js

--- cookies/app.js
+++ cookies/app.js
@@ -1,1 +1,21 @@
 
+"use strict";
+
+import http from 'http';
+import { handleRequest } from './routing.js';
+
+
+
+function main()
+{
+	const PORT = 3000;
+
+    let server = http.createServer();
+    server.listen(PORT);
+    
+    console.log(`Web app listening on port ${PORT}`);
+
+	server.on("request", handleRequest);
+}
+
+main();
