b261a27bd5479c77179282e22d6ac389c49d1a41
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-30 21:52:18 +0200

    Update reset-controller.js

--- cookies/controllers/reset-controller.js
+++ cookies/controllers/reset-controller.js
@@ -1,1 +1,28 @@
 
+import http from "http";
+import pug from "pug";
+
+import { Database } from "../data/Database.js";
+
+
+
+/**
+ * @param {http.IncomingMessage} request 
+ * @param {http.ServerResponse} response 
+*/
+export function handleResetRequest(request, response, context)
+{
+    let db = new Database();
+    
+    db.resetDatabase();
+    
+    let renderedContent = pug.renderFile("./templates/resetPage.pug", context);
+    
+    response.writeHead(200, 
+                       {
+                           "Content-Type": "text/html",
+                           "Set-Cookie": ["auth=false; max-age=0",
+                                          "username=; max-age=0"]
+                       });
+    response.end(renderedContent);
+}
