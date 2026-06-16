54cb6c125df5aca27ca21b803ab3307be7beabab
Ciro Pizza <ciropizza2002@gmail.com>
2026-06-16 22:42:07 +0200

    Update homepage-controller.js

--- web session/controllers/homepage-controller.js
+++ web session/controllers/homepage-controller.js
@@ -1,1 +1,18 @@
 
+import http from "http"
+import pug from "pug"
+
+
+
+/**
+ * Handle requests to the homepage
+ * @param {http.IncomingMessage} request 
+ * @param {http.ServerResponse} response 
+*/
+export function handleHomepageRequest(request, response, context={})
+{
+    let renderedContent = pug.renderFile("./templates/homePage.pug", context);
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(renderedContent);
+}
