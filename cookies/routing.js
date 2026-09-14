3acfe17a0956f6299f37817f5a5eb79d6cab7705
Ciro Pizza <ciropizza2002@gmail.com>
2026-09-14 02:20:35 +0200

    Update routing.js

--- cookies/routing.js
+++ cookies/routing.js
@@ -1,6 +1,6 @@
-
 import http from 'http';
 import fs from 'fs';
+
 import { handleHomepageRequest } from './controllers/homepage-controller.js';
 import { handleAuthRequest, handleSignUpRequest, handleLogoutRequest } from './controllers/auth-controller.js';
 import { handleResetRequest } from './controllers/reset-controller.js';
@@ -10,76 +10,78 @@ import { handleError, checkUserAuthentication } from './utils.js';
 
 /**
  * Callback function to handle HTTP requests
- * @param {http.IncomingMessage} request 
- * @param {http.ServerResponse} response 
+ * @param {http.IncomingMessage} request
+ * @param {http.ServerResponse} response
 */
 export function handleRequest(request, response)
 {
-	let [isAuthenticated, username] = checkUserAuthentication(request);
+   let [isAuthenticated, username] = checkUserAuthentication(request);
 
-	let context = 
-	{
-		isAuthenticated: isAuthenticated,
-		username: username
-	};
+   let context =
+   {
+      isAuthenticated: isAuthenticated,
+      username: username
+   };
 
-	switch (request.url)
-	{
-		case "/":
-			handleHomepageRequest(request, response, context);
-			break;
-		
-		case "/auth":
-			handleAuthRequest(request, response, context);
-			break;
-		
-		case "/signup":
-			handleSignUpRequest(request, response, context);
-			break;
-		
-		case "/logout":
-			handleLogoutRequest(request, response, context);
-			break;
+   switch (request.url)
+   {
+      case "/":
+         handleHomepageRequest(request, response, context);
+         break;
 
-		case "/reset":
-			handleResetRequest(request, response, context);
-			break;
-		
-		case "/todo":
-			handleTodoListRequest(request, response, context);
-			break;
-		
-		case "/css/bootstrap.css":
-			fs.readFile('./static/css/bootstrap.css', function(err, data) { 
-																		      response.writeHead(200, {'Content-Type': 'text/css'});
-																		      response.end(data, 'utf-8');
-																		  }); 
-			break;
-	
-		case "/js/bootstrap.bundle.js":
-			fs.readFile('./static/js/bootstrap.bundle.js', function(err, data) { 
-																		           response.writeHead(200, {'Content-Type': 'text/javascript'});
-																		           response.end(data, 'utf-8');
-																		  	   }); 
-			break;
-		
-		case "favicon.ico":
-			fs.readFile('./favicon.ico', function(err, data) { 
-																	    response.writeHead(200, {'Content-Type': 'image/ico'});
-																		response.end(data, 'binary');
-																	}); 
-			break;
+      case "/auth":
+         handleAuthRequest(request, response, context);
+         break;
 
-		case "/img/node.png":
-			fs.readFile('./static/img/node.png', function(err, data) {
-																	     response.writeHead(200, {'Content-Type': 'image/png'});
-																	     response.end(data, 'binary');
-																	 });
-			break;
-		
+      case "/signup":
+         handleSignUpRequest(request, response, context);
+         break;
 
-		default:
-			handleError(request, response, 404, "Web page not found");
-	}
+      case "/logout":
+         handleLogoutRequest(request, response, context);
+         break;
+
+      case "/reset":
+         handleResetRequest(request, response, context);
+         break;
+
+      case "/todo":
+         handleTodoListRequest(request, response, context);
+         break;
+
+      case "/css/bootstrap.css":
+         fs.readFile('./static/css/bootstrap.css', function(err, data)
+                                                   {
+                                                      response.writeHead(200, {'Content-Type': 'text/css'});
+                                                      response.end(data, 'utf-8');
+                                                   });
+         break;
+
+      case "/js/bootstrap.bundle.js":
+         fs.readFile('./static/js/bootstrap.bundle.js', function(err, data)
+                                                        {
+                                                           response.writeHead(200, {'Content-Type': 'text/javascript'});
+                                                           response.end(data, 'utf-8');
+                                                        });
+         break;
+
+      case "favicon.ico":
+         fs.readFile('./favicon.ico', function(err, data)
+                                      {
+                                         response.writeHead(200, {'Content-Type': 'image/ico'});
+                                         response.end(data, 'binary');
+                                      });
+         break;
+
+      case "/img/node.png":
+         fs.readFile('./static/img/node.png', function(err, data)
+                                              {
+                                                 response.writeHead(200, {'Content-Type': 'image/png'});
+                                                 response.end(data, 'binary');
+                                              });
+         break;
+
+      default:
+         handleError(request, response, 404, "Page not found!")
+   }
 }
-
