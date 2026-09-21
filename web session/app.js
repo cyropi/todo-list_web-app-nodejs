9f154ff463b9dffbb3195c807e3595d6e3522bab
Ciro Pizza <ciropizza2002@gmail.com>
2026-09-21 05:25:01 +0200

    Update app.js

--- web session/app.js
+++ web session/app.js
@@ -1,1 +1,18 @@
+"use strict";
 
+import http from "http";
+import { handleRequest } from "./routing.js";
+
+
+function main()
+{
+   const PORT = 3000;
+
+   let server = http.createServer();
+   server.listen(PORT);
+   server.on("request", handleRequest);
+
+   console.log(`Web app listening on port ${PORT}`);
+}
+
+main();
