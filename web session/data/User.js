3025c4e4635f537528b67b394c446100a1abaffc
Ciro Pizza <ciropizza2002@gmail.com>
2026-09-23 23:42:45 +0200

    Update User.js

--- web session/data/User.js
+++ web session/data/User.js
@@ -1,15 +1,1 @@
 
-import { createHash } from "crypto";
-
-
-export class User 
-{
-    constructor(username, password) 
-    {
-        let hash = createHash("sha256");
-
-        this.username = username;
-        this.password = hash.update(password)
-                            .digest("hex");
-    }
-}      
